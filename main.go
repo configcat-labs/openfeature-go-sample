534c8188061d35c5a452edb06bfb97000a4c5123
Zayyad Muhammad Sani <zayyadmuhammed700@gmail.com>
2024-01-22 15:35:15 +0100

    First commit

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"net/http"
+
+	configcat "github.com/configcat/go-sdk/v8"
+	"github.com/gin-gonic/gin"
+)
+
+type course struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Author string  `json:"author"`
+	Price  float64 `json:"price"`
+}
+
+var courses = []course{
+	{ID: "1", Title: "Intro to HTML", Author: "Guy Person", Price: 49.99},
+	{ID: "2", Title: "Intro to JavaScript", Author: "Mister Coder", Price: 59.99},
+	{ID: "3", Title: "Building APIs with Go", Author: "Sir Programmer", Price: 60.99},
+}
+
+func main() {
+	client := configcat.NewClient("YOUR-SDK-KEY")
+
+	router := gin.Default()
+	router.GET("/courses", func(c *gin.Context) {
+		getCoursesEnabled := client.GetBoolValue("get_courses_enabled", false, nil)
+
+		if getCoursesEnabled {
+			c.IndentedJSON(http.StatusOK, courses)
+		} else {
+			c.IndentedJSON(http.StatusNotFound, gin.H{
+				"code":    http.StatusNotFound,
+				"message": "Not Found",
+			})
+		}
+	})
+
+	router.Run("localhost:8000")
+}
